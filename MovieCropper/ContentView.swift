b8768a8b98538f31c2a0ba5751c3ee4a5e00d527
natmark <natmark0918@gmail.com>
2024-05-25 15:21:24 +0900

    Add new xcode proj

---
+++ MovieCropper/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MovieCropper
+//
+//  Created by atsuyan on 2024/05/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MovieCropper/MovieCropperApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieCropperApp.swift
+//  MovieCropper
+//
+//  Created by atsuyan on 2024/05/25.
+//
+
+import SwiftUI
+
+@main
+struct MovieCropperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
